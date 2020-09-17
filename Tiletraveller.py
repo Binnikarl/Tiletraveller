3d244bbb136ad8d9a402314bc225f3b8af9a279e
Brynjar Karl Ævarsson <Brynjarka19@ru.is>
2020-09-17 14:26:06 +0000

    bætt við algorithma

--- Tiletraveller.py
+++ Tiletraveller.py
@@ -1,1 +1,4 @@
-print("Hello")
+# Þufum fyrst tvær breytur sem segja okkur hvar upphafsstaður er, 
+# og mun svo segja okkur hvar leikmaðurinn er staddur eftir því sem að við færum okkur
+# Þurfum að búa til fall, tekur inn hvert leikmaður vill ferðast og updater staðsetninguna eða ekki(tjékkar hvort það virki)
+# Þurfum að skilgreina öll "Tiles" og hvert leikmaðurinn má ferðast í því tilteknu "Tile"
